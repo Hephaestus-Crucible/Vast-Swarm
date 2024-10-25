60d6b1421791bd46ae549a1bb37fbd275af9adf1
Airlesspayload6 <62572773+Airlesspayload6@users.noreply.github.com>
2024-10-25 11:43:28 -0400

    Getting closer
    A few things need to be fixed but I am getting closer to desired end-goal:
    -The camera is no longer clipping through walls
    -The camera is clipping through the character model
    -Next goal will be to prevent the camera from clipping through model without compromising the orientation
    -Also need to work on EyeAngles, as currently there is no access to horizontal eye movement (yaw?)

--- vast_swarm/code/Components/VastSwarmPlayer.cs
+++ vast_swarm/code/Components/VastSwarmPlayer.cs
@@ -1,6 +1,5 @@
 using Sandbox;
 using Sandbox.Citizen;
-using System.Security.Cryptography.X509Certificates;
 
 public sealed class VastSwarmPlayer : Component
 {
@@ -21,7 +20,6 @@ public sealed class VastSwarmPlayer : Component
 	/// </summary>
 	[Property]
 	[Category( "Stats" )]
-	//Creating a slider to create limits for attributes
 	[Range( 0f, 400f, 1f )]
 	public float WalkSpeed { get; set; } = 120f;
 
@@ -30,7 +28,6 @@ public sealed class VastSwarmPlayer : Component
 	/// </summary>
 	[Property]
 	[Category( "Stats" )]
-	//Creating a slider to create limits for attributes
 	[Range( 0f, 800f, 1f )]
 	public float RunSpeed { get; set; } = 250f;
 
@@ -39,68 +36,94 @@ public sealed class VastSwarmPlayer : Component
 	/// </summary>
 	[Property]
 	[Category( "Stats" )]
-	//Creating a slider to create limits for attributes
 	[Range( 0f, 1000f, 10f )]
 	public float JumpStrength { get; set; } = 400f;
 
-	/// <summary>
-	/// Where the camera rotates around and the aim originates from
-	/// </summary>
 	[Property]
 	public Vector3 EyePosition { get; set; }
-	//Vector created to determine position of character view in correlation to the world instead of controller
-	public Vector3 EyeWorldPosition => Transform.Local.PointToWorld( EyePosition );
 
-	//EyeAngles is used to keep track of what the character is currently looking at
+	public Vector3 EyeWorldPosition => Transform.Local.PointToWorld( EyePosition );
 	public Angles EyeAngles { get; set; }
+
 	Transform _initialCameraTransform;
 
+	private void AdjustModelForFirstPerson()
+	{
+		if (Components.TryGet<SkinnedModelRenderer>(out var modelRenderer))
+		{
+			// Loop through the body groups and match the name
+			foreach (var bodyGroup in modelRenderer.Model.BodyParts)
+			{
+				// Check if the body group name is "head_lod0"
+				if (bodyGroup.Name == "head_lod0")
+				{
+					// Get the index of the body group
+					// Need to continue working on this part of the code
+					// For some reason BodyGroups does not seem to be accesible
+					// Might be because it is defined in a different way
+					// Or I am looking in the wrong direction all together
+					// Overall the task here is keep the camera from clipping within the head of character
+					//int headIndex = modelRenderer.Model.BodyParts.IndexOf( bodyGroup );
+
+					// Set the body group to invisibile (0)
+					//modelRenderer.SetBodyGroup( headIndex, 0 ); // Make the head invisible
+					break;
+				}
+			}
+		}
+	}
 
 	protected override void OnUpdate()
 	{
 		base.OnUpdate();
 
-		//We add AnalogLook to eye angles to determine how far the mouse has moved since the last frame
-		EyeAngles += Input.AnalogLook;
-		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -20f, 20f ) );
-		//Transform.Rotation will now update the player view depending on where the EyeAngles.yaw has been moved to (left to right)
-		WorldRotation = Rotation.FromYaw( EyeAngles.yaw );
+		if ( Camera == null ) return;
 
-
-		//This is the new and "improved" version of ray-tracing developed for the first person POV
-		if ( Camera != null )
-		{
-			//Get the current eye position and eye angles for first-person view
-			var eyePosition = EyeWorldPosition;
-			var eyeRotation = EyeAngles;
-
-			// Defining a distance for collision detection with ray-tracing
-			float traceDistance = 10f; // Adjust this distance as needed 
-
-			//Performing ray-tracing in the first-person perspective, 
-			//Tracing a short distance forward to detect potential collisions or obstructions.
-			var forwardTrace = Scene.Trace.Ray( eyePosition, eyePosition + eyeRotation.Forward * traceDistance ) 
-				.Size( 5f ) //Setting the size of trace
-				.IgnoreGameObjectHierarchy( GameObject ) //Ignoring the player's own model
-				.WithoutTags( "player" ) //Avoiding collision with entities tagged as "player"
-				.Run();
-
-			if ( forwardTrace.Hit ) //Adjusting the camera position based on the trace result to avoid clipping through walls
-			{
-				Camera.Transform.Position = forwardTrace.EndPosition; //Moving the camera to the trace hit position, ensuring it doesn't intersect with obstacles
-			}
-
-			//Drawing a gizmo sphere at the final camera position for debugging purposes
-			DrawGizmoAtEyePosition( Camera.Transform.Position );
-		}
-			
+		UpdateEyeRotation();
+		AdjustModelForFirstPerson();
+		PerformRayTracingForCamera();
 	}
 
-	private void DrawGizmoAtEyePosition(Vector3 position)
+	private void UpdateEyeRotation()
+	{
+		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -20f, 20f ) );
+		WorldRotation = Rotation.FromYaw( EyeAngles.yaw );
+	}
+
+	private void PerformRayTracingForCamera()
+	{
+		//var eyePosition = EyeWorldPosition;
+		//var eyeRotation = EyeAngles;
+
+		float traceDistance = 5f; // Defining a close range for first-person clipping detection
+		Vector3 eyePosition = EyeWorldPosition; 
+
+		// Perform ray-trace directly forward from eye position
+		var forwardTrace = Scene.Trace.Ray( eyePosition, eyePosition + EyeAngles.Forward * traceDistance )
+			.Size( 5f ) // Camera "size" for trace
+			.IgnoreGameObjectHierarchy( GameObject ) // Ignore the player's own model
+			.WithoutTags( "player" ) // Avoid collision with entities tagged as "player"
+			.Run();
+
+		if ( forwardTrace.Hit )
+		{
+			// Reposition camera slightly back from obstacle to prevent clipping
+			Camera.WorldPosition = forwardTrace.EndPosition - EyeAngles.Forward * 0.1f; //Back off a small amount
+		}
+		else
+		{
+			// Place camera at the expected eye position if no obstacle
+			Camera.WorldPosition = eyePosition;
+		}
+
+		DrawGizmoAtEyePosition( Camera.WorldPosition );
+	}
+
+	private void DrawGizmoAtEyePosition( Vector3 position )
 	{
 		Gizmo.Draw.LineSphere( position, 10f );
 	}
-	
 
 	protected override void OnFixedUpdate()
 	{
@@ -108,57 +131,48 @@ public sealed class VastSwarmPlayer : Component
 
 		if ( Controller == null ) return;
 
-		//Determines whether the player is pressing the run key, in this instance "Shift", and changes the WalkSpeed to run speed. 
-		//Otherwise, WalkSpeed is default
+		UpdateMovement();
+		UpdateAnimation();
+	}
+
+	private void UpdateMovement()
+	{
 		var wishSpeed = Input.Down( "Run" ) ? RunSpeed : WalkSpeed;
-		//wishVelocity takes the player's inputs and multiplies it with the wishSpeed
-		//This will allow functionality with WASD along with any controller, essentially creating 
-		//universal inputs. Transform.Rotation is multiplied to change velocity vector with player's rotation
-		//Normal vector is used to keep the diagonal movement speed the same as vertical/horizontal
 		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * WorldRotation;
 
-		//Accelerating controller based off of the wishVelocity
 		Controller.Accelerate( wishVelocity );
 
-		//This if statement will keep player from sliding after they have stopped inputting movement by applying friction
 		if ( Controller.IsOnGround )
 		{
-			//Default controller acceleration
 			Controller.Acceleration = 10f;
 			Controller.ApplyFriction( 5f );
-
-
-			//Check if "Jump" is pressed, in this case Space, which is only determined in the frame the input is pressed
-			//The punch method is used to push the player up, determined by jump strength, which is applied to the velocity vector instantaneously. 
-			//Useful since the controller likes to stick the player to the ground
-			if ( Input.Pressed( "Jump" ) )
-			{
-				Controller.Punch( Vector3.Up * JumpStrength );
-
-				//If jump is used, then the jump animation is triggered.
-				if ( Animator != null )
-					Animator.TriggerJump();
-			}
-
+			HandleJump();
 		}
-		//If controller is not on the ground, this will apply gravity to bring character back down
 		else
 		{
-			//Acceleration is cut down to half, to prevent strafing when midair
 			Controller.Acceleration = 5f;
 			Controller.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta;
 		}
-		//After determining what we want the controller to do, Controller.Move() is used to apply the complicated stuff
-		Controller.Move();
 
-		//This helps the controller understand when the character is back on the ground
-		//Creating smooth transition of animation
+		Controller.Move();
+	}
+
+	private void HandleJump()
+	{
+		if ( Input.Pressed( "Jump" ) )
+		{
+			Controller.Punch( Vector3.Up * JumpStrength );
+			Animator?.TriggerJump();
+		}
+	}
+
+	private void UpdateAnimation()
+	{
 		if ( Animator != null )
 		{
 			Animator.IsGrounded = Controller.IsOnGround;
 			Animator.WithVelocity( Controller.Velocity );
 		}
-
 	}
 
 	protected override void OnStart()
@@ -187,5 +201,5 @@ public sealed class VastSwarmPlayer : Component
 	{
 		base.OnDestroy();
 	}
-
 }
+
