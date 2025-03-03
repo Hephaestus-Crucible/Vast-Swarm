db5f17c9989f2d1c0fcb93ed9188a559bfd1e1cd
Airlesspayload6 <62572773+Airlesspayload6@users.noreply.github.com>
2025-03-03 14:52:37 -0500

    Initializing enemy class

---
+++ vast_swarm/code/Components/VastSwarmEnemy.cs
@@ -0,0 +1,9 @@
+using Sandbox;
+using System.Collections.ObjectModel;
+
+public partial class EnemySpawner : Entity
+{
+    [Property] public int MaxEnemies { get; set; } = 10; //Max enemies allowed will be 10 
+    [Property]
+    [Property]
+}
