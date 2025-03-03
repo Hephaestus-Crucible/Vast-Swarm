3f2e4733b2894ac02207ff8ba294b24431fd3db8
Airlesspayload6 <62572773+Airlesspayload6@users.noreply.github.com>
2025-03-03 15:14:10 -0500

    Working on enemy class

--- vast_swarm/code/Components/VastSwarmEnemy.cs
+++ vast_swarm/code/Components/VastSwarmEnemy.cs
@@ -1,9 +1,35 @@
 using Sandbox;
 using System.Collections.ObjectModel;
 
-public partial class EnemySpawner : Entity
+public partial class EnemySpawner : Component
 {
     [Property] public int MaxEnemies { get; set; } = 10; //Max enemies allowed will be 10 
-    [Property]
-    [Property]
+	[Property] public float SpawnInterval { get; set; } = 5.0f; //Time between enemies spawning in
+    [Property] public List<Vector3> SpawnPoints { get; set; } = new(); //List of spawn locations
+
+	private TimeSince _timeSinceLastSpawn;
+	private List<Enemy> _activeEnemies = new();
+
+	public override void OnStart()
+	{
+		_timeSinceLastSpawn = 0;
+	}
+
+	protected override void OnUpdate()
+	{
+		//Checking if we can spawn a new enemy
+		if(_timeSinceLastSpawn > SpawnInterval && _activeEnemies.Count < MaxEnemies)
+		{
+			SpawnEnemy();
+			_timeSinceLastSpawn = 0;
+		}
+
+		//Removing any dead enemies
+		_activeEnemies.RemoveAll( equals => !equals.isValid() );
+	}
+
+
+
+	
+
 }
