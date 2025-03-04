cd3a0214939eb0efd90ffe57fbc8e7d272a304a1
Tanner <tjlang1120@gmail.com>
2025-03-03 19:30:19 -0500

    Added Interaction Bounds
    Added a trigger collider that detects if im in it to allow me to interact with the barrier, raising it. I need to add a UI hint to "Press E to Interact" next.

--- vast_swarm/code/Components/BarrierInteract.cs
+++ vast_swarm/code/Components/BarrierInteract.cs
@@ -1,17 +1,67 @@
 using Sandbox;
+using Sandbox.UI;
 
-public sealed class BarrierInteract : Component
+public sealed class BarrierInteract : Component, Component.ITriggerListener, Component.ICollisionListener
 {
 	[Property] public Barrier Barrier;
 
+	private bool isPlayerInRange = false;
+	//private BarrierUI barrierUI;
+
+
+	protected override void OnStart()
+	{
+		/*barrierUI = Game.ActiveScene.GetAllComponents<BarrierUI>().FirstOrDefault();
+		if ( barrierUI == null )
+		{
+			barrierUI = new BarrierUI();
+			Game.ActiveScene.AddComponent<BarrierUI>();
+		}*/
+
+		var collider = GameObject.Components.Get<BoxCollider>();
+		if ( collider != null )
+		{
+			collider.IsTrigger = true;
+		}
+		else
+		{
+			Log.Warning( "Barrier Interact requires a collider component set as a trigger" );
+		}
+	}
+
 	protected override void OnUpdate()
 	{
+		if ( !isPlayerInRange )
+		{
+			return;
+		}
 		if ( Input.Pressed( "use" ) ) // 'E' or interact button
 		{
 			if ( Barrier != null )
 			{
 				Barrier.ActivateBarrier();
+				//barrierUI.IsVisible = false;
 			}
+		}
+	}
+
+	public void OnTriggerEnter(Collider Other )
+	{
+		if ( Other.GameObject.Tags.Has( "player" ))				//ensure that its a player
+		{
+			isPlayerInRange = true;
+			//barrierUI.Message = "Press E to remove barrier";
+			//barrierUI.IsVisible = true;
+		}
+
+	}
+
+	public void OnTriggerExit( Collider Other )				//ensure that its a player
+	{
+		if( Other.GameObject.Tags.Has( "player" ) )
+		{
+			isPlayerInRange = false;
+			//barrierUI.IsVisible = false;
 		}
 	}
 }
